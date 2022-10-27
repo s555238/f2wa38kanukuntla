df76154c8f8e81493fc23bf82d6e59948a4cf134
Sumanth Reddy Kanukuntla <sumanthkanukuntla16@gmail.com>
2022-10-26 20:21:31 -0500

    added users and mydata functionality

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var times=0
-
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-
-  times+=1;
-  res.send(`User accesses are:${times}`);
+  res.render('mydata', { title: 'Sumanth Kanukuntla' });
 });
 
 module.exports = router;
